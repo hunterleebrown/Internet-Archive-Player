523f456556c0f080f41a6dfcf0c40ba74b7d20bb
Hunter Lee Brown <hunterleebrown@gmail.com>
2023-09-11 14:44:35 -0700

    Test of automatic TestFlight changeling notes for Testing

---
+++ ci_post_xcodebuild.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [[ $CI_WORKFLOW == "TestFlight" ]]; then
+  pushd ..
+    mkdir TestFlight
+    pushd TestFlight
+      for locale in en-GB en-US; do
+        git fetch --deepen 3 && git log -3 --pretty=format:"%s" > WhatToTest.$locale.txt
+      done
+    popd
+  popd
+fi
