dc25a4625f9c6487fa6d7f9d5af69a3a21b2a024
Hunter Lee Brown <hunterleebrown@gmail.com>
2023-09-15 14:42:14 -0700

    remove merge messages

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -5,7 +5,7 @@
 if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
   TESTFLIGHT_DIR_PATH=../TestFlight
   mkdir $TESTFLIGHT_DIR_PATH
-  git fetch --deepen 3 && git log latest_build..HEAD --pretty=format:"%s" > $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+  git fetch --deepen 3 && git log --no-merges --pretty=format:"%s" latest_build..HEAD > $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
   git push --delete https://${GIT_AUTH}@github.com/hunterleebrown/Internet-Archive-Player.git latest_build
   git tag -d latest_build
   git tag latest_build
