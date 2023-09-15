117849e3221efa79f119a86003d5eb90c9c25981
Hunter Lee Brown <hunterleebrown@gmail.com>
2023-09-15 14:43:04 -0700

    clean up

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#        git fetch --deepen 3 && git log main..HEAD --pretty=format:"%s" > WhatToTest.$locale.txt
-
 if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
   TESTFLIGHT_DIR_PATH=../TestFlight
   mkdir $TESTFLIGHT_DIR_PATH
@@ -10,5 +8,4 @@ if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
   git tag -d latest_build
   git tag latest_build
   git push --tags https://${GIT_AUTH}@github.com/hunterleebrown/Internet-Archive-Player.git
-#  git push origin latest_build -f
 fi
