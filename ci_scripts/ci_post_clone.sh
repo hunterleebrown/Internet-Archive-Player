bdacf9875710ee5837d1c0adc3af910bab16dd20
Hunter Lee Brown <hunterleebrown@gmail.com>
2023-09-15 14:00:13 -0700

    testing smarter workflows for pushing tags

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,6 +5,6 @@
 #
 #  Created by Hunter Lee Brown on 9/15/23.
 #  
-if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
+#if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
     git fetch origin tag latest_build -f
-fi
+#fi
